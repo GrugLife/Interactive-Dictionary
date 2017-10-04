272111628f09aa7481dc9e72fae99f91db3dfe55
GrugLife <gwylie1@gmail.com>
2017-10-04 10:24:23 -0700

    refactored

--- interactiveDict.py
+++ interactiveDict.py
@@ -1,13 +1,13 @@
-import json, difflib
+import json 
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def definition(word):
     if word in data:
         return data[word]
-    elif difflib.get_close_matches(word, data.keys())[0] in data:
-        word = difflib.get_close_matches(word, data.keys(), n=3)[0]
-        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean {} instead?".format(get_close_matches(word, data.keys())[0])
     return "Could not find the word.  Please check the spelling"
 
 word = input("What word would you like to find the defintion(s) for: ").lower()
